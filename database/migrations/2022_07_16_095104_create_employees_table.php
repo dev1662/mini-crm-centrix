b74f5f3870288f32cdc750c043a690568bd974a9
Dev Sindhwani <dev16sindh@gmail.com>
2022-07-16 20:57:15 +0530

    Update 2022_07_16_095104_create_employees_table.php

--- database/migrations/2022_07_16_095104_create_employees_table.php
+++ database/migrations/2022_07_16_095104_create_employees_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->unsignedBigInteger('company');
+            $table->string('email');
+            $table->string('phone');
             $table->timestamps();
         });
     }
